dc2ea28cfc13f77d9af251cbc4c9620d725a8139
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-15 18:05:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let emptyArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return emptyArray;
+}
+ function johnLennonFacts(facts) {
+    let lennyFacts = [];
+    let i = 0;
+    while (i < facts.length) {
+      lennyFacts.push(facts[i] +"!!!");
+      i++;
+     }
+     return lennyFacts;
+   }
+  function iLoveTheBeatles(num) {
+    let blankArray = [];
+    do {
+      blankArray.push("I love the Beatles!");
+      num++;
+    } while (num < 15);
+    return blankArray;
+  }
